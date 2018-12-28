59ca1e30a77f3d78c4bdbf78e830e9122991cea7
Chris Zuber <shgysk8zer0@gmail.com>
2018-12-28 11:38:55 -0800

    Misc enhancements

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-(async () => {
+(async (configFile = './config.json') => {
 	const WebSocket = require('ws');
 	const http      = require('http');
 	const readline  = require('readline');
 	const mysql     = require('mysql');
-	const config    = require('./config.json');
+	const config    = require(configFile);
 
 	class Socket extends WebSocket.Server {
 		constructor(...args) {
@@ -12,7 +12,8 @@
 
 		static async connect(server = http.createServer(), ...args) {
 			const socket = new Socket(...args);
-			const db = mysql.createConnection(config.mysql);
+			const db     = mysql.createConnection(config.mysql);
+
 			db.config.queryFormat = function(query, values) {
 				if (!values) return query;
 				return query.replace(/:(\w+)/g, (txt, key) => {
@@ -22,15 +23,19 @@
 					return txt;
 				});
 			};
+
 			db.connect();
 			server.on('upgrade', socket.handleUpgrade);
 			console.log(`Listening on ${socket.options.host || '*'}:${socket.options.port}`);
 			const rl = readline.createInterface(process.stdin, process.stdout);
+
 			socket.on('connection', (client, con) => {
 				console.log('New connection established');
 				client.send(JSON.stringify({message: 'Hello. How may I help you?', event: 'message'}));
+
 				rl.on('line', txt => client.send(JSON.stringify({message: txt, event: 'message'})));
 				rl.on('close', () => process.exit(0));
+
 				client.on('message', msg => {
 					try {
 						const {time, event, text} = JSON.parse(msg);
