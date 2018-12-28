7cd1a85137c145c0051fc80b875ec86d7549c418
Chris Zuber <shgysk8zer0@gmail.com>
2018-12-28 11:34:16 -0800

    Add mysql package & ignored config file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-(async (...args) => {
+(async () => {
 	const WebSocket = require('ws');
 	const http      = require('http');
 	const readline  = require('readline');
+	const mysql     = require('mysql');
+	const config    = require('./config.json');
 
 	class Socket extends WebSocket.Server {
 		constructor(...args) {
@@ -10,10 +12,21 @@
 
 		static async connect(server = http.createServer(), ...args) {
 			const socket = new Socket(...args);
+			const db = mysql.createConnection(config.mysql);
+			db.config.queryFormat = function(query, values) {
+				if (!values) return query;
+				return query.replace(/:(\w+)/g, (txt, key) => {
+					if (values.hasOwnProperty(key)) {
+						return this.escape(values[key]);
+					}
+					return txt;
+				});
+			};
+			db.connect();
 			server.on('upgrade', socket.handleUpgrade);
 			console.log(`Listening on ${socket.options.host || '*'}:${socket.options.port}`);
 			const rl = readline.createInterface(process.stdin, process.stdout);
-			socket.on('connection', client => {
+			socket.on('connection', (client, con) => {
 				console.log('New connection established');
 				client.send(JSON.stringify({message: 'Hello. How may I help you?', event: 'message'}));
 				rl.on('line', txt => client.send(JSON.stringify({message: txt, event: 'message'})));
@@ -22,8 +35,17 @@
 					try {
 						const {time, event, text} = JSON.parse(msg);
 						const date	= new Date(time);
+						const timestamp = Date.parse(date);
 						switch(event) {
 						case 'message':
+							db.query(
+								'INSERT INTO `messages` (`text`, `timestamp`,`ip`) VALUES (:text, :timestamp, :ip)',
+								{
+									text,
+									timestamp,
+									ip: con.connection.remoteAddress
+								}
+							);
 							console.log(`[${date.toLocaleString()}] ${text}`);
 							break;
 						default: throw new Error(`Unhandled event: "${event}"`);
@@ -37,7 +59,5 @@
 		}
 	}
 
-	Socket.connect(http.createServer(), ...args);
-})({
-	port: 3000,
-});
+	Socket.connect(http.createServer(), config.socket);
+})();
