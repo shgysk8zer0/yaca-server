6dc8a9ce17054106784ae044e4dfbf9821117be3
Chris Zuber <shgysk8zer0@gmail.com>
2019-01-02 15:46:13 -0800

    Fix errors

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 (async (configFile = './config.json') => {
 	const WebSocket  = require('./WebSocket.js');
 	const http       = require('http');
-	const readline   = require('readline');
+	// const readline   = require('readline');
 	// const MySQL      = require('./MySQL.js');
 	const config     = require(configFile);
 
 	const server     = http.createServer();
 	const socket     = new WebSocket(config.socket);
-	const mysql      = new MySQL(config.mysql);
+	// const mysql      = new MySQL(config.mysql);
 
 	server.on('upgrade', socket.handleUpgrade);
 	console.log(`Listening on ${socket.options.host || '*'}:${socket.options.port}`);
-
+	let [client1, client2] = [null, null]; // Avoid linting error
 	for await ([client1, client2] of socket.clientPairs()) {
-		client1.on('message', msg => {
-			const
-		});
+		client1.on('message', msg => client2.send(msg));
 		client1.on('close',   ()  => client2.close(1000, 'Other party exited'));
 		client2.on('message', msg => client1.send(msg));
 		client2.on('close',   ()  => client1.close(1000, 'Other party exited'));
